4cbf23e351981ae0ac73af0da44158ca0ece7e7f
Egg2023 <142619947+Egg2023@users.noreply.github.com>
2023-10-09 22:41:17 -0300

    Delete Ejercicio 1 directory

--- Ejercicio 1/ejercicio1.js
+++
@@ -1,9 +0,0 @@
-// Escribir un algoritmo en el cual se consulte al usuario que ingrese ¿cómo está el día de
-// hoy? (soleado, nublado, lloviendo). A continuación, mostrar por pantalla un mensaje que
-// indique “El día de hoy está ...”, completando el mensaje con el dato que ingresó el usuario. 
-
-const respuesta = prompt('¿cómo está el día de hoy? (soleado, nublado, lloviendo).');
-
-alert(`El día de hoy está ${respuesta}`);
-
-document.write(`El día de hoy está ${respuesta}`);
